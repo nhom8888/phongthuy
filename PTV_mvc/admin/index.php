e9e5a2515522a5b281ba4af0f392f83c01fceb93
anhdaden151@outlook.com.vn <anhdaden151@outlook.com.vn>
2021-04-15 07:45:46 +0700

    del

--- PTV_mvc/admin/index.php
+++
@@ -1,9 +0,0 @@
-﻿<?php include 'inc/header.php';?>
-<?php include 'inc/sidebar.php';?>
-        <div class="grid_10">
-            <div class="box round first grid">
-                <h2>Trang chủ</h2>
-               
-            </div>
-        </div>
-<?php include 'inc/footer.php';?>
