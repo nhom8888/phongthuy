e39c674de2e283c01b89d9738e25c432e59e30f6
Khuất Tuấn Anhhhhhhh <75654034+tuananh0222222222@users.noreply.github.com>
2021-08-17 18:49:01 +0700

    Delete PHONG THUY_MVC directory

--- PHONG THUY_MVC/database/conn.php
+++
@@ -1,5 +0,0 @@
-<?php
-//database connection
-($GLOBALS["___mysqli_ston"] = mysqli_connect("localhost","root","","blog_admin_db"));
- //host,user,password,database
-?>
--- PHONG THUY_MVC/database/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$database_username = 'root';//user name
-	$database_password = '';//password
-	$pdo_conn = new PDO( 'mysql:host=localhost;dbname=blog_admin_db', $database_username, $database_password );
-?>
